62c387763ceab255dcf532ef2f7f012f00bbf2f1
Emil Gustafsson <gustafssonemil94@gmail.com>
2020-09-10 17:27:41 +0200

    Update reward attributes

--- server/models/reward.js
+++ server/models/reward.js
@@ -2,6 +2,7 @@ const { Schema } = require("mongoose");
 var Schema = mongoose.Schema;
 var rewardSchema = new Schema(
     {
+        account: {type: Schema.Types.ObjectId, ref: 'Account'},
         rewardName: String,
         rewardDesc: String,
         isBought: Boolean,
