1ff8140001ee770dd65e89130ac7b80f95fc3d28
simengs <simengs@student.chalmers.se>
2020-09-11 15:14:50 +0200

    Updated account schema and controller

--- server/models/Child.js
+++ server/models/Child.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 var accountSchema = new Schema({
     username            : String,
     account_password    : String,
-    parent_password     : String,
-    balance             : Number
+    balance             : Number,
+    parent              : String
 });
 
-module.exports = mongoose.model('accounts', accountSchema);
+module.exports = mongoose.model('children', accountSchema);
