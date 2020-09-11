4d20ab8d305c4dfe1fdcabfe18dee832d0927687
matekd <matekd@student.chalmers.se>
2020-09-11 15:39:38 +0200

    Update Schema child, quest and reward

--- server/models/child.js
+++ server/models/child.js
@@ -2,10 +2,12 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var accountSchema = new Schema({
-    username            : String,
-    account_password    : String,
-    balance             : Number,
-    parent              : String
+    username         : String,
+    password         : String,
+    balance          : Number,
+    parent           : { type: Schema.Types.ObjectId, ref: 'Parent'},
+    completed_quests : [{type: Schema.Types.ObjectId, ref: 'Quest'}],
+    bought_rewards   : [{type: Schema.Types.ObjectId, ref: 'Reward'}]
 });
 
 module.exports = mongoose.model('children', accountSchema);
