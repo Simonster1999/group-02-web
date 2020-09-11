24ececcefb50093065fb8ed131e5977d8ed33205
matekd <matekd@student.chalmers.se>
2020-09-11 17:23:02 +0200

    Add constraints to Schema child and reward

--- server/models/child.js
+++ server/models/child.js
@@ -1,13 +1,13 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var accountSchema = new Schema({
-    username         : String,
-    password         : String,
-    balance          : Number,
-    parent           : { type: Schema.Types.ObjectId, ref: 'Parent'},
+var childSchema = new Schema({
+    username         : {type: String, required: true, unique: true},
+    password         : {type: String, required: true},
+    balance          : {type: Number, default: 0},
+    parent           : {type: Schema.Types.ObjectId, ref: 'Parent'},
     completed_quests : [{type: Schema.Types.ObjectId, ref: 'Quest'}],
     bought_rewards   : [{type: Schema.Types.ObjectId, ref: 'Reward'}]
 });
 
-module.exports = mongoose.model('children', accountSchema);
+module.exports = mongoose.model('children', childSchema);
--- server/models/reward.js
+++ server/models/reward.js
@@ -2,10 +2,10 @@ var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
 var rewardSchema = new Schema({
-    reward_name : String,
-    reward_desc : String,
-    is_bought   : Boolean,
-    price       : Number,
+    reward_name : {type: String, required: true},
+    reward_desc : {type: String},
+    is_bought   : {type: Boolean, default: false},
+    price       : {type: Number, required: true},
     parent      : {type: Schema.Types.ObjectId, ref: 'Parent'}
 });
 
