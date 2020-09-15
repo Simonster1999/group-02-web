073d580e45c8e7a06bb6165073b3b6a16504d753
matekd <matekd@student.chalmers.se>
2020-09-15 15:03:23 +0200

    Edit quest schema

--- server/models/quest.js
+++ server/models/quest.js
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema;
 var questSchema = new Schema({
     quest_name   : { type: String, required: true },
     money_bounty : { type: Number, required: true },
-    is_completed : { type: Boolean, required: true, default: false },
+    is_completed : { type: Boolean, default: false },
     quest_desc   : { type: String },
     date         : { type: Date, required: true },
     parent       : { type: Schema.Types.ObjectId, ref: 'Parent', required: true}
