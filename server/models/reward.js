f54f741720a5bfaedb36e3133ed2d4badad46229
matekd <matekd@student.chalmers.se>
2020-09-24 17:19:06 +0200

    Change reward and quest to store a child reference instead of child having two lists

--- server/models/reward.js
+++ server/models/reward.js
@@ -6,7 +6,8 @@ var rewardSchema = new Schema({
     reward_desc : {type: String, default: ''},
     is_bought   : {type: Boolean, default: false},
     price       : {type: Number, required: true},
-    parent      : {type: Schema.Types.ObjectId, ref: 'Parent', required: true}
+    parent      : {type: Schema.Types.ObjectId, ref: 'Parent', required: true},
+    bought_by   : { type: Schema.Types.ObjectId, ref: 'Child'}
 });
 
 module.exports = mongoose.model('rewards', rewardSchema);
