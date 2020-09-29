24219c230c86d699e2ce0749492e032015cd08a9
simengs <simengs@student.chalmers.se>
2020-09-29 10:55:24 +0200

    Added edit function to quests

--- client/src/components/QuestItem.vue
+++ client/src/components/QuestItem.vue
@@ -1,11 +1,16 @@
 <template>
        <div>
            <h1 class="p-3 mb-2 bg-dark text-dark"/>
+          <b-container class="bv-example-row">
+           <b-row>
+             <b-col><b-button variant="warning" v-on:click="$emit('put-quest', quest._id)">Edit</b-button></b-col>
+             <b-col><b-button variant="danger" v-on:click="$emit('del-quest', quest._id)">X</b-button></b-col>
+           </b-row>
+          </b-container>
            <p>Name: {{quest.quest_name}}</p>
            <p>Quest Description: {{quest.quest_desc}}</p>
            <p>Reward: {{quest.money_bounty}}</p>
            <p>Date: {{ quest.date }} </p>
-           <b-button variant="danger" v-on:click="$emit('del-quest', quest._id)">X</b-button>
         </div>
 </template>
 
--- client/src/views/ParentQuests.vue
+++ client/src/views/ParentQuests.vue
@@ -9,7 +9,7 @@
         <b-form-input v-model="money_bounty" placeholder="Enter reward"></b-form-input>
         </b-col>
         <b-col> <b-sidebar bg-variant="light" visible="true" right="true" width="35%" no-header-close > <div  v-for="quest in quests" v-bind:key="quest._id">
-        <quest-item v-bind:quest="quest" v-on:del-quest="deleteQuest"/>
+        <quest-item v-bind:quest="quest" v-on:del-quest="deleteQuest" v-on:put-quest="putQuest"/>
         </div> </b-sidebar></b-col>
         </b-row>
     </b-container>
@@ -21,7 +21,7 @@ import { Api } from '@/Api'
 import QuestItem from '@/components/QuestItem.vue'
 export default {
   beforeCreate: function () { document.body.className = 'parent-quest' },
-  name: 'quests',
+  name: 'parent-quests',
   components: {
     QuestItem
   },
@@ -49,15 +49,6 @@ export default {
     }
   },
   methods: {
-    getMessage() {
-      Api.get('/')
-        .then(response => {
-          this.message = response.data.message
-        })
-        .catch(error => {
-          this.message = error
-        })
-    },
     deleteQuest(id) {
       Api.delete(`/quests/${id}`)
         .then(reponse => {
@@ -66,6 +57,15 @@ export default {
         })
         .catch(error => {
           console.error(error)
+        })
+    },
+    putQuest(id) {
+      Api.put(`/quests/${id}`,
+        {
+          quest_name: this.name,
+          quest_desc: this.quest_desc,
+          money_bounty: this.money_bounty,
+          date: this.value
         })
     },
     createQuest() {
@@ -77,7 +77,6 @@ export default {
           date: this.value,
           parent: '5f60b206ea77e02c3c712dc2'
         })
-      Api.get('/quests')
     }
   }
 }
